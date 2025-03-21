696e091f606fa2e47ae63b672b93fafb5039caf3
gkaragoz <gkaragoz96@gmail.com>
2025-03-21 23:57:36 +0300

    Add npm start to package.json

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
 	console.log(`WebApp is running at http://localhost:${PORT}`);
 });
